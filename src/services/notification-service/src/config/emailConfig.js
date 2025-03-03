b60c7836e38c0643b17f5cde6640bd9afae619b2
adeel-mahmood-ansari <adeel.mahmoodansari@gmail.com>
2025-03-03 18:49:23 +0500

    Made event-service and notification-service

---
+++ src/services/notification-service/src/config/emailConfig.js
@@ -0,0 +1,22 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false, // Use `true` for port 465, `false` for other ports
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    },
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("Email service error:", error);
+    } else {
+        console.log("Email service is ready to send emails.");
+    }
+});
+
+module.exports = transporter;
